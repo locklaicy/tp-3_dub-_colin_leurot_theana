e990d826bc63cef7d95d7367d63c2fed9ab6a4fa
Théana L. <theana.leurot@gmail.com>
2026-05-14 10:10:38 -0400

    avancées js

--- pizza.js
+++ pizza.js
@@ -1,12 +1,13 @@
+// Espace Théana
+
 /*2.2*/
 // Formulaire pizza
 const pizzaForm = document.getElementById("formpizza");
 const pizza = document.createElement("form");
 
-
 // Margherita - quantité
-const margheritaDiv = document.createElement("div");
-margheritaDiv.setAttribute("id", "margherita")
+/*const margheritaDiv = document.createElement("div");
+margheritaDiv.setAttribute("id", "margherita");
 
 const margheritaInput = document.createElement("input");
 margheritaInput.setAttribute("type", "checkbox");
@@ -27,40 +28,38 @@ margheritaDiv.appendChild(margheritaNumber);
 // Margherita - garniture
 const garnituresMargheritaDiv = document.createElement("div");
 
-const garnituresLabel = document.createElement("label");
-garnituresLabel.textContent = "Garnitures : ";
+const garnituresMargheritaLabel = document.createElement("label");
+garnituresMargheritaLabel.textContent = "Garnitures : ";
 
-const fromageLabel = document.createElement("label");
-fromageLabel.textContent = "Fromage";
-const fromageInput = document.createElement("input");
-fromageInput.setAttribute("type", "checkbox");
-fromageInput.setAttribute("name", "fromage");
-fromageLabel.appendChild(fromageInput)
+const fromageMargheritaLabel = document.createElement("label");
+fromageMargheritaLabel.textContent = "Fromage";
+const fromageMargheritaInput = document.createElement("input");
+fromageMargheritaInput.setAttribute("type", "checkbox");
+fromageMargheritaInput.setAttribute("name", "fromageMargherita");
+fromageMargheritaLabel.appendChild(fromageMargheritaInput);
 
-const champignonsLabel = document.createElement("label");
-champignonsLabel.textContent = "Champignons";
-const champignonsInput = document.createElement("input");
-champignonsInput.setAttribute("type", "checkbox");
-champignonsInput.setAttribute("name", "champignons");
-champignonsLabel.appendChild(champignonsInput)
+const champignonsMargheritaLabel = document.createElement("label");
+champignonsMargheritaLabel.textContent = "Champignons";
+const champignonsMargheritaInput = document.createElement("input");
+champignonsMargheritaInput.setAttribute("type", "checkbox");
+champignonsMargheritaInput.setAttribute("name", "champignonsMargherita");
+champignonsMargheritaLabel.appendChild(champignonsMargheritaInput);
 
-const oignonsLabel = document.createElement("label");
-oignonsLabel.textContent = "Oignons";
-const oignonsInput = document.createElement("input");
-oignonsInput.setAttribute("type", "checkbox");
-oignonsInput.setAttribute("name", "oignons");
-oignonsLabel.appendChild(oignonsInput)
+const oignonsMargheritaLabel = document.createElement("label");
+oignonsMargheritaLabel.textContent = "Oignons";
+const oignonsMargheritaInput = document.createElement("input");
+oignonsMargheritaInput.setAttribute("type", "checkbox");
+oignonsMargheritaInput.setAttribute("name", "oignonsMargherita");
+oignonsMargheritaLabel.appendChild(oignonsMargheritaInput);
 
-
-garnituresMargheritaDiv.appendChild(garnituresLabel);
-garnituresMargheritaDiv.appendChild(fromageLabel);
-garnituresMargheritaDiv.appendChild(champignonsLabel);
-garnituresMargheritaDiv.appendChild(oignonsLabel);
-
+garnituresMargheritaDiv.appendChild(garnituresMargheritaLabel);
+garnituresMargheritaDiv.appendChild(fromageMargheritaLabel);
+garnituresMargheritaDiv.appendChild(champignonsMargheritaLabel);
+garnituresMargheritaDiv.appendChild(oignonsMargheritaLabel);
 
 // Pepperonni - quantité
 const pepperoniDiv = document.createElement("div");
-pepperoniDiv.setAttribute("id", "pepperoni")
+pepperoniDiv.setAttribute("id", "pepperoni");
 
 const pepperoniInput = document.createElement("input");
 pepperoniInput.setAttribute("type", "checkbox");
@@ -88,34 +87,31 @@ const fromagePepperonniLabel = document.createElement("label");
 fromagePepperonniLabel.textContent = "Fromage";
 const fromagePepperonniInput = document.createElement("input");
 fromagePepperonniInput.setAttribute("type", "checkbox");
-fromagePepperonniInput.setAttribute("name", "fromage");
-fromagePepperonniLabel.appendChild(fromagePepperonniInput)
+fromagePepperonniInput.setAttribute("name", "fromagePepperoni");
+fromagePepperonniLabel.appendChild(fromagePepperonniInput);
 
 const champignonsPepperonniLabel = document.createElement("label");
 champignonsPepperonniLabel.textContent = "Champignons";
 const champignonsPepperonniInput = document.createElement("input");
 champignonsPepperonniInput.setAttribute("type", "checkbox");
-champignonsPepperonniInput.setAttribute("name", "champignons");
-champignonsPepperonniLabel.appendChild(champignonsPepperonniInput)
+champignonsPepperonniInput.setAttribute("name", "champignonsPepperoni");
+champignonsPepperonniLabel.appendChild(champignonsPepperonniInput);
 
-const oliveLabel = document.createElement("label");
-oliveLabel.textContent = "Oignons";
-const oliveInput = document.createElement("input");
-oliveInput.setAttribute("type", "checkbox");
-oliveInput.setAttribute("name", "olive");
-oliveLabel.appendChild(oignonsInput)
-
+const olivePepperoniLabel = document.createElement("label");
+olivePepperoniLabel.textContent = "Oignons";
+const olivePepperoniInput = document.createElement("input");
+olivePepperoniInput.setAttribute("type", "checkbox");
+olivePepperoniInput.setAttribute("name", "olivePepperoni");
+olivePepperoniLabel.appendChild(olivePepperoniInput);
 
 garnituresPepperoniDiv.appendChild(garnituresPepperonniLabel);
 garnituresPepperoniDiv.appendChild(fromagePepperonniLabel);
 garnituresPepperoniDiv.appendChild(champignonsPepperonniLabel);
-garnituresPepperoniDiv.appendChild(oliveLabel);
-
-
+garnituresPepperoniDiv.appendChild(olivePepperoniLabel);
 
 // Végétarienne - quantité
 const vegeDiv = document.createElement("div");
-vegeDiv.setAttribute("id", "vege")
+vegeDiv.setAttribute("id", "vege");
 
 const vegeInput = document.createElement("input");
 vegeInput.setAttribute("type", "checkbox");
@@ -144,32 +140,30 @@ oignonsVegeLabel.textContent = "Oignons";
 const oignonsVegeInput = document.createElement("input");
 oignonsVegeInput.setAttribute("type", "checkbox");
 oignonsVegeInput.setAttribute("name", "oignons");
-oignonsVegeLabel.appendChild(oignonsVegeInput)
+oignonsVegeLabel.appendChild(oignonsVegeInput);
 
 const champignonsVegeLabel = document.createElement("label");
 champignonsVegeLabel.textContent = "Champignons";
 const champignonsVegeInput = document.createElement("input");
 champignonsVegeInput.setAttribute("type", "checkbox");
 champignonsVegeInput.setAttribute("name", "champignons");
-champignonsVegeLabel.appendChild(champignonsPepperonniInput)
+champignonsVegeLabel.appendChild(champignonsPepperonniInput);
 
 const oliveVegeLabel = document.createElement("label");
 oliveVegeLabel.textContent = "Oignons";
 const oliveVegeInput = document.createElement("input");
 oliveVegeInput.setAttribute("type", "checkbox");
 oliveVegeInput.setAttribute("name", "olive");
-oliveVegeLabel.appendChild(oignonsInput)
-
+oliveVegeLabel.appendChild(oignonsInput);
 
 garnituresVegeDiv.appendChild(garnituresVegeLabel);
 garnituresVegeDiv.appendChild(oignonsVegeLabel);
 garnituresVegeDiv.appendChild(champignonsVegeLabel);
 garnituresVegeDiv.appendChild(oliveVegeLabel);
 
-
 // Sicilicienne - quantité
 const siciliDiv = document.createElement("div");
-siciliDiv.setAttribute("id", "sicili")
+siciliDiv.setAttribute("id", "sicili");
 
 const siciliInput = document.createElement("input");
 siciliInput.setAttribute("type", "checkbox");
@@ -198,22 +192,21 @@ fromageSiciliLabel.textContent = "Fromage";
 const fromageSiciliInput = document.createElement("input");
 fromageSiciliInput.setAttribute("type", "checkbox");
 fromageSiciliInput.setAttribute("name", "fromage");
-fromageSiciliLabel.appendChild(fromageSiciliInput)
+fromageSiciliLabel.appendChild(fromageSiciliInput);
 
 const champignonsSiciliLabel = document.createElement("label");
 champignonsSiciliLabel.textContent = "Champignons";
 const champignonsSiciliInput = document.createElement("input");
 champignonsSiciliInput.setAttribute("type", "checkbox");
 champignonsSiciliInput.setAttribute("name", "champignons");
-champignonsSiciliLabel.appendChild(champignonsSiciliInput)
+champignonsSiciliLabel.appendChild(champignonsSiciliInput);
 
 const oignonsSiciliLabel = document.createElement("label");
 oignonsSiciliLabel.textContent = "Oignons";
 const oignonsSiciliInput = document.createElement("input");
 oignonsSiciliInput.setAttribute("type", "checkbox");
 oignonsSiciliInput.setAttribute("name", "oignons");
-oignonsSiciliLabel.appendChild(oignonsSiciliInput)
-
+oignonsSiciliLabel.appendChild(oignonsSiciliInput);
 
 garnituresSiciliDiv.appendChild(garnituresSiciliLabel);
 garnituresSiciliDiv.appendChild(fromageSiciliLabel);
@@ -229,25 +222,60 @@ pizza.appendChild(garnituresPepperoniDiv);
 pizza.appendChild(vegeDiv);
 pizza.appendChild(garnituresVegeDiv);
 pizza.appendChild(siciliDiv);
-pizza.appendChild(garnituresSiciliDiv)
-
+pizza.appendChild(garnituresSiciliDiv);
 
 pizzaForm.appendChild(pizza);
-
-
+*/
 /*2.3*/
 // Mode de paiement
 
-/*const modePaiementDiv = document.createElement ("div");
-modePaiementDivsetAttribut("id", "modePaiementDiv");
+const modePaiementForm = document.getElementById("formpaiement");
+
+const modePaiementDiv = document.createElement("div");
+modePaiementDiv.setAttribute("id", "modePaiementDiv");
 
 const modePaiementLabel = document.createElement("label");
 modePaiementLabel.textContent = "Mode de paiement : ";
-*/
 
+const carteEnLigneDiv = document.createElement("div");
 
+const carteEnLigneInput = document.createElement("input");
+carteEnLigneInput.setAttribute("type", "radio");
+carteEnLigneInput.setAttribute("name", "carteEnLigneInput");
 
+const carteEnLigneLabel = document.createElement("label");
+carteEnLigneLabel.textContent = "Carte de crédit (en ligne)";
 
+const cartePorteDiv = document.createElement("div");
+
+const cartePorteInput = document.createElement("input");
+cartePorteInput.setAttribute("type", "radio");
+cartePorteInput.setAttribute("name", "cartePorteInput");
+
+const cartePorteLabel = document.createElement("label");
+cartePorteLabel.textContent = "Débit/Crédit (à la porte)";
+
+const especeDiv = document.createElement("div");
+
+const especeInput = document.createElement("input");
+especeInput.setAttribute("type", "radio");
+especeInput.setAttribute("name", "especeInput");
+
+const especeLabel = document.createElement("label");
+especeLabel.textContent = "Espèces (à la porte)";
+
+carteEnLigneDiv.appendChild(carteEnLigneInput);
+carteEnLigneDiv.appendChild(carteEnLigneLabel);
+cartePorteDiv.appendChild(cartePorteInput);
+cartePorteDiv.appendChild(cartePorteLabel);
+especeDiv.appendChild(especeInput);
+especeDiv.appendChild(especeLabel);
+
+modePaiementDiv.appendChild(carteEnLigneDiv);
+modePaiementDiv.appendChild(cartePorteDiv);
+modePaiementDiv.appendChild(especeDiv);
+
+modePaiementForm.appendChild(modePaiementDiv);
 
 /* Sauter ligne */
 document.getElementById("formpizza").innerHTML += "<br>";
@@ -261,10 +289,11 @@ document.getElementById("container").innerHTML += "<br>";
 // Recuperer le conteneur du formulaire
 const clientContainer = document.getElementById("formclient");
 
-// Creer un formulaire
+// Créer un formulaire
 const clientForm = document.createElement("client");
 
-// Creéer un champ de texte pour le nom
+// Créer un champ de texte pour le nom
+
 const nomLabel = document.createElement("label");
 nomLabel.textContent = "Nom : ";
 const nomInput = document.createElement("input");
@@ -272,8 +301,6 @@ nomInput.setAttribute("type", "text");
 nomInput.setAttribute("name", "name");
 nomInput.setAttribute("size", "15");
 nomLabel.appendChild(nomInput);
-
-
 
 const prenomLabel = document.createElement("label");
 prenomLabel.textContent = "Prénom : ";
@@ -308,9 +335,6 @@ clientForm.appendChild(adresseLabel);
 // Ajouter le formulaire au conteneur
 clientContainer.appendChild(clientForm);
 
-
-
-
 /*2.5*/
 
 // Recuperer le conteneur du formulaire
@@ -328,7 +352,6 @@ nomprenomInput.setAttribute("name", "name");
 nomprenomInput.setAttribute("size", "15");
 nomprenomLabel.appendChild(nomprenomInput);
 
-
 const telephonelivraisonLabel = document.createElement("label");
 telephonelivraisonLabel.textContent = "Téléphone :  ";
 const telephonelivraisonInput = document.createElement("input");
@@ -345,8 +368,6 @@ adresselivraisonInput.setAttribute("name", "name");
 adresselivraisonInput.setAttribute("size", "15");
 adresselivraisonLabel.appendChild(adresselivraisonInput);
 
-
-
 // Ajouter les champs au formulaire
 livraisonForm.appendChild(nomprenomLabel);
 livraisonForm.appendChild(telephonelivraisonLabel);
@@ -355,23 +376,51 @@ livraisonForm.appendChild(adresselivraisonLabel);
 // Ajouter le formulaire au conteneur
 livraisonContainer.appendChild(livraisonForm);
 
-
-
 /*2.6*/
 
-const container = document.getElementById('container');
+const boutonContainer = document.getElementById("boutonContainer");
 
-const btn = document.createElement('button');
-btn.textContent = 'Commander';
-btn.id = 'myBtn';
-btn.className = 'my-button';
+const bouton = document.createElement("button");
+bouton.textContent = "Commander";
+bouton.id = "bouton";
 
-btn.addEventListener('click', function () {
-  const msg = document.createElement('p');
-  msg.textContent = 'Button clicked!';
-  container.appendChild(msg);
-});
+boutonContainer.appendChild(bouton);
 
-container.appendChild(btn);
+// Fonctions
 
+// Génerer la facture
+/*function genererFacture() {
+  const facture = [];
+  const prixPizza = 12;
+  const prixGarniture = 2;
 
+  // Vérification des éléments
+  // Pizzas
+  if (margheritaInput.checked && margheritaNumber.value > 0) {
+    facture.push ({
+      item: "Pizza Margherita",
+      prix: prixUnitairePizza,
+      quantite: parseInt(margheritaNumber.value),
+    });
+  }
+
+  if (fromageMargheritaInput.checked) { facture.push ({item: "Fromage Margherita", prix: prixGarniture,});}
+  if (champignonsMargheritaInput.checked) { facture.push({item: "Champignong Margherita", prix: prixGarniture,})};
+  if (oignonsMargheritaInput.checked) { facture.push({item: "Oignons Margherita", prix: prixGarniture,});}
+
+  if (pepperoniInput.checked && pepperoniNumber.value > 0) {
+    facture.push ({
+      item: "Pizza Pepperoni",
+      prix: prixUnitairePizza,
+      quantite: parseInt(pepperoniNumber.value),
+    });
+  }
+
+  if (fromagePepperoniInput.checked) { facture.push ({item: "Fromage Pepperoni", prix: prixGarniture,});}
+  if (champignonsPepperoniInput.checked) { facture.push({item: "Champignong Pepperoni", prix: prixGarniture,})};
+  if (olivePepperoniInput.checked) { facture.push({item: "Oignons Pepperoni", prix: prixGarniture,});}
+}
+// Appel de la fonction pour génerer la facture avec le bouton
+bouton.addEventListener("click", function () {
+  genererFacture();
+})*/
